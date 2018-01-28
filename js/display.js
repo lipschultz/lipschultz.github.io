187a699330cb0a2c4df9d0972b3910c36e14d62c
Michael Lipschultz <michael.lipschultz@gmail.com>
2018-01-28 17:38:50 -0500

    Start with code hidden

--- js/display.js
+++ js/display.js
@@ -1,19 +1,18 @@
 function toggleCodeDisplay() {
     var button = document.getElementById("toggleCodeButton");
+    var displayValue = '';
     if (button.value == 'Show Code') {
         button.value = 'Hide Code';
+        displayValue = 'block';
     }
     else {
         button.value = 'Show Code';
+        displayValue = 'none';
     }
 
     var codeObjects = document.getElementsByClassName("language-python");
     var i;
     for (i = 0; i < codeObjects.length; i++) {
-        if (codeObjects[i].style.display === "none") {
-            codeObjects[i].style.display = "block";
-        } else {
-            codeObjects[i].style.display = "none";
-        }
+        codeObjects[i].style.display = displayValue;
     }
 }
